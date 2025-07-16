b4f1cbaa9462160450b88e7839bd69edceaac86a
Renato Kolbe <rkolbe@vsb.net.br>
2025-07-16 10:38:56 -0300

    Exemplo Tabela TipoPessoa
    Signed-off-by: Renato Kolbe <rkolbe@vsb.net.br>

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\TipoPessoaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::apiResource('tipos-pessoa', TipoPessoaController::class);
+
+// Route::get('/', function () {
+//     return response()->json(['message' => 'Welcome to the API!']);
+// });
